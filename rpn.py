35d3302ed7958a03d2e50f2332d5c037ad668eab
Hubert Jo <hubertjo@umich.edu>
2019-10-30 20:02:55 -0400

    Working sub and add

--- rpn.py
+++ rpn.py
@@ -4,32 +4,23 @@ def calculate(arg):
     stack = list()
     for token in arg.split():
         if token == '+':
-            arg1 = stack.pop()
             arg2 = stack.pop()
+            arg1 = stack.pop()
             result = arg1 + arg2
+            stack.append(result)
+        elif token == '-':
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = arg1 - arg2
             stack.append(result)
         else:
             stack.append(int(token))
 
-        print(stack)
+        #print(stack)
+    if len(stack) != 1:
+        raise TypeError('malformed input')
     return stack.pop()
-    
-def main():
-    while True:
-        calculate(input('rpn calc> '))
-
-
-if __name__ == '__main__':
-    main()
-#!/usr/bin/env python3
-
-def calculate(arg):
-    pass
 
 def main():
     while True:
-        calculate(input('rpn calc> '))
-
-
-if __name__ == '__main__':
-    main()
+        calculate(input("rpn calc> "))
